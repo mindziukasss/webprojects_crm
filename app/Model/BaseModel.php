8e399a476e924762661b5491093100c784178873
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-04-21 14:43:18 +0300

    update base model

--- app/Model/BaseModel.php
+++ app/Model/BaseModel.php
@@ -11,8 +11,38 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class BaseModel extends Model
 {
     use SoftDeletes;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * Boot function from laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = (string) $model->generateNewId();
+        });
+    }
+
+    public function generateNewId()
+    {
+        if (isset($this->attributes['id'])) {
+            return $this->attributes['id'];
+        }
+
+        return uuid4();
+    }
+
 }
